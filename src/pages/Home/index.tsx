06d3a0e9c1d2e9f5ccf9b051afc08ae1762b7e17
LeeonardoVargas <leu1607@hotmail.com>
2020-09-21 11:11:14 -0300

    Listando de forma aleatória os filmes das sessões

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -28,21 +28,27 @@ const Home: React.FC = () => {
   const [loading, setLoading] = useState(true);
 
   const apiRoutes: { name: string; route: string }[] = [
-    { name: 'Originais do Netflix', route: '/discover/tv/?with_network=213&' },
-    { name: 'Recomendados para Você', route: '/trending/all/week?' },
-    { name: 'Em Alta', route: '/movie/top_rated?&' },
+    { name: 'Em alta', route: '/tv/popular?' },
+    { name: 'Populares na Netflix', route: '/trending/all/week?' },
+    { name: 'Melhores Avaliados', route: '/movie/top_rated?' },
+    { name: 'Lançamentos', route: '/movie/now_playing?' },
     { name: 'Ação', route: '/discover/movie?with_genres=28&' },
     { name: 'Comédia', route: '/discover/movie?with_genres=35&' },
-    { name: 'Terror', route: '/discover/movie?with_genres=27&' },
-    { name: 'Romance', route: '/discover/movie?with_genres=10749&' },
+    // { name: 'Romance', route: '/discover/movie?with_genres=10749&' },
+    // { name: 'Documentário', route: '/discover/movie?with_genres=99&' },
   ];
 
   useEffect(() => {
-    const URL_LANGUAGE_AND_KEY = `language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`;
+    const URL_LANGUAGE_AND_KEY = `language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}&page=`;
 
-    const urlsAxios = apiRoutes.map(({ route }) =>
-      api.get(route.concat(URL_LANGUAGE_AND_KEY)),
-    );
+    const urlsAxios = apiRoutes.map(({ route }, index) => {
+      let pageRandom = '1';
+      // Somente as três primeiras listas são randômicas
+      if (index < 3) pageRandom = (Math.random() * (5 - 1) + 1).toString();
+
+      const URL = route.concat(URL_LANGUAGE_AND_KEY).concat(pageRandom);
+      return api.get(URL);
+    });
 
     if (sectionsMovies.length === 0) {
       Promise.all([...urlsAxios])
